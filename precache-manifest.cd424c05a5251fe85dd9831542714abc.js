28adae71b93805909a469d0577d56c5b860b3326
Gary Rivera <a.gary.rivera@gmail.com>
2021-07-22 14:21:12 -0700

    Updates

---
+++ precache-manifest.cd424c05a5251fe85dd9831542714abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd0617feb0c93adaba64a327d7c0174",
+    "url": "/productiv/index.html"
+  },
+  {
+    "revision": "f2f20e4327483ddd0f1e",
+    "url": "/productiv/static/css/2.7118e3f5.chunk.css"
+  },
+  {
+    "revision": "869828bac31bde317926",
+    "url": "/productiv/static/css/main.2dd59b60.chunk.css"
+  },
+  {
+    "revision": "f2f20e4327483ddd0f1e",
+    "url": "/productiv/static/js/2.854f531c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/productiv/static/js/2.854f531c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869828bac31bde317926",
+    "url": "/productiv/static/js/main.35aeb9aa.chunk.js"
+  },
+  {
+    "revision": "0d2b6fd2d724e31f558a",
+    "url": "/productiv/static/js/runtime-main.35a1ce50.js"
+  }
+]);
